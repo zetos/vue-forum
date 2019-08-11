f696679ae12acf2b0db00c8e2f42cc4304496d12
Zetos Lima <cesar_manoel@hotmail.com>
2019-08-10 23:55:22 -0300

    theme css

--- src/App.vue
+++ src/App.vue
@@ -4,18 +4,15 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    <router-view />
+    <div class="container">
+      <router-view />
+    </div>
   </div>
 </template>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+@import 'assets/css/style.css';
+
 #nav {
   padding: 30px;
 }
