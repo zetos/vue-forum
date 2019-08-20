1815b8aa3cf6b801ba4e2a70a072ff4e80ff9943
Zetos Lima <cesar_manoel@hotmail.com>
2019-08-19 22:40:43 -0300

    refactor ViewThreadRead into new components

--- src/views/ViewThreadRead.vue
+++ src/views/ViewThreadRead.vue
@@ -1,46 +1,19 @@
 <template>
   <div class="col-large push-top">
     <h1>{{ thread.title }}</h1>
-
-    <div class="post-list">
-      <div v-for="postId in thread.posts" :key="postId.key" class="post">
-        <div class="user-info">
-          <a href="#" class="user-name">{{
-            users[posts[postId].userId].name
-          }}</a>
-
-          <a href="#">
-            <img
-              class="avatar-large"
-              :src="users[posts[postId].userId].avatar"
-              alt="user avatar"
-            />
-          </a>
-
-          <p class="desktop-only text-small">107 posts</p>
-        </div>
-
-        <div class="post-content">
-          <div>
-            <p>
-              {{ posts[postId].text }}
-            </p>
-          </div>
-        </div>
-
-        <div class="post-date text-faded">
-          {{ posts[postId].publishedAt }}
-        </div>
-      </div>
-    </div>
+    <PostList :posts="posts" />
   </div>
 </template>
 
 <script>
 import sourceData from '@/data';
+import PostList from '@/components/PostList';
 
 export default {
   name: 'ViewThreadRead',
+  components: {
+    PostList
+  },
   props: {
     id: {
       required: true,
@@ -49,10 +22,16 @@ export default {
   },
   data() {
     return {
-      thread: sourceData.threads[this.id],
-      posts: sourceData.posts,
-      users: sourceData.users
+      thread: sourceData.threads[this.id]
     };
+  },
+  computed: {
+    posts() {
+      const postIds = Object.values(this.thread.posts);
+      return Object.values(sourceData.posts).filter(post =>
+        postIds.includes(post['.key'])
+      );
+    }
   }
 };
 </script>
