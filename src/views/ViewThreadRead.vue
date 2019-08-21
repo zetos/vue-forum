f86e661378418e5e4a94c3365bffdf45732f2d2c
Zetos Lima <cesar_manoel@hotmail.com>
2019-08-21 00:00:53 -0300

    addPost method

--- src/views/ViewThreadRead.vue
+++ src/views/ViewThreadRead.vue
@@ -2,6 +2,20 @@
   <div class="col-large push-top">
     <h1>{{ thread.title }}</h1>
     <PostList :posts="posts" />
+    <form @submit.prevent="addPost">
+      <div class="form-group">
+        <textarea
+          name=""
+          id=""
+          cols="30"
+          rows="10"
+          v-model="newPostText"
+        ></textarea>
+      </div>
+      <div class="form-actions">
+        <button class="btn-blue">Submit post</button>
+      </div>
+    </form>
   </div>
 </template>
 
@@ -22,7 +36,8 @@ export default {
   },
   data() {
     return {
-      thread: sourceData.threads[this.id]
+      thread: sourceData.threads[this.id],
+      newPostText: ''
     };
   },
   computed: {
@@ -31,6 +46,24 @@ export default {
       return Object.values(sourceData.posts).filter(post =>
         postIds.includes(post['.key'])
       );
+    }
+  },
+  methods: {
+    addPost() {
+      const postId = 'greatPost' + Math.random();
+
+      const post = {
+        text: this.newPostText,
+        publishedAt: Math.floor(Date.now() / 1000),
+        threadId: this.id,
+        userId: 'ALXhxjwgY9PinwNGHpfai6OWyDu2',
+        '.key': postId
+      };
+
+      this.$set(sourceData.posts, postId, post);
+      this.$set(this.thread.posts[postId], postId, postId);
+      this.$set(sourceData.users[post.userId].posts, postId, postId);
+      this.newPostText = '';
     }
   }
 };
