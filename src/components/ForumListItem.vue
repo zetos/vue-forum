07bcbcd3e3bdcb1848ea76af72b0840e63e56653
Zetos Lima <cesar_manoel@hotmail.com>
2019-08-27 22:17:20 -0300

    ForumList & ForumListItem components

---
+++ src/components/ForumListItem.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="forum-listing">
+    <div class="forum-details">
+      <router-link
+        :to="{ name: 'ViewForum', params: { id: forum['.key'] } }"
+        class="text-xlarge"
+        >{{ forum.name }}</router-link
+      >
+      <p>{{ forum.description }}</p>
+    </div>
+
+    <div class="threads-count">
+      <p>
+        <span class="count">{{ threadsCount }}</span>
+        {{ threadsCount === 1 ? 'thread' : 'threads' }}
+      </p>
+    </div>
+
+    <div class="last-thread">
+      <!-- TODO -->
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    forum: {
+      required: true,
+      type: Object
+    }
+  },
+  computed: {
+    threadsCount() {
+      return this.forum.threads ? Object.values(this.forum.threads).length : 0;
+    }
+  }
+};
+</script>
+
+<style></style>
