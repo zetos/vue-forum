1202083b2a79f3b773bb7c2558b6513f261b4209
Zetos Lima <cesar_manoel@hotmail.com>
2019-08-28 19:22:06 -0300

    vuex actions & mutations implementation

--- src/store.js
+++ src/store.js
@@ -7,6 +7,28 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: sourceDate,
-  mutations: {},
-  actions: {}
+  mutations: {
+    // mutations args: (objectToAddNewProp, {propertyName, valueOfProperty})
+    setPost(state, { postId, post }) {
+      Vue.set(state.posts, postId, post);
+    },
+    appendPostToThread(state, { postId, threadId }) {
+      const thread = state.threads[threadId];
+      Vue.set(thread.posts, postId, postId);
+    },
+    appendPostToUser(state, { postId, userId }) {
+      const user = state.users[userId];
+      Vue.set(user.posts, postId, postId);
+    }
+  },
+  actions: {
+    createPost(context, post) {
+      const postId = 'greatPost' + Math.random();
+      post['.key'] = postId;
+
+      context.commit('setPost', { post, postId });
+      context.commit('appendPostToThread', { threadId: post.threadId, postId });
+      context.commit('appendPostToUser', { userId: post.userId, postId });
+    }
+  }
 });
--- src/views/ViewThreadRead.vue
+++ src/views/ViewThreadRead.vue
@@ -12,12 +12,11 @@
       </span>
     </p>
     <PostList :posts="posts" />
-    <PostEditor @save="addPost" :threadId="id" />
+    <PostEditor :threadId="id" />
   </div>
 </template>
 
 <script>
-
 import PostList from '@/components/PostList';
 import PostEditor from '@/components/PostEditor';
 
@@ -43,15 +42,6 @@ export default {
       return Object.values(this.$store.state.posts).filter(post =>
         postIds.includes(post['.key'])
       );
-    }
-  },
-  methods: {
-    addPost({ post }) {
-      const postId = post['.key'];
-
-      this.$set(this.$store.state.posts, postId, post);
-      this.$set(this.thread.posts, postId, postId);
-      this.$set(this.$store.state.users[post.userId].posts, postId, postId);
     }
   }
 };
