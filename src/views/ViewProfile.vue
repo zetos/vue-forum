3b0ac76f34743a63d1f3c6511b042357c9f17293
Zetos Lima <cesar_manoel@hotmail.com>
2019-08-30 18:45:52 -0300

    UserProfileCard component

--- src/views/ViewProfile.vue
+++ src/views/ViewProfile.vue
@@ -1,45 +1,10 @@
 <template>
   <div class="flex-grid">
-    <div class="col-3 push-top">
-      <div class="profile-card">
-        <p class="text-center">
-          <img :src="user.avatar" alt="User Avatar" class="avatar-xlarge" />
-        </p>
-
-        <h1 class="title">{{ user.username }}</h1>
-
-        <p class="text-lead">{{ user.name }}</p>
-
-        <p class="text-justify">
-          <span v-if="user.bio">{{ user.bio }}</span>
-          <span v-else>No bio specified.</span>
-        </p>
-
-        <span class="online">{{ user.username }} is online</span>
-
-        <div class="stats">
-          <span>{{ userPostsCount }} posts</span>
-          <span>{{ userThreadsCount }} threads</span>
-        </div>
-
-        <hr />
-
-        <p v-if="user.website" class="text-large text-center">
-          <i class="fa fa-globe"></i>
-          <a :href="user.website">{{ user.website }}</a>
-        </p>
-      </div>
-
-      <p class="text-xsmall text-faded text-center">
-        Member since june 2003, last visited 4 hours ago
-      </p>
-
-      <div class="text-center">
-        <hr />
-        <a href="#" class="btn-green btn-small">Edit Profile</a>
-      </div>
-    </div>
-
+    <UserProfileCard
+      :user="user"
+      :userPostsCount="userPostsCount"
+      :userThreadsCount="userThreadsCount"
+    />
     <div class="col-7 push-top">
       <div class="profile-header">
         <span class="text-lead">
@@ -57,12 +22,15 @@
 
 <script>
 import PostList from '@/components/PostList';
+import UserProfileCard from '@/components/UserProfileCard';
+
 import { mapGetters } from 'vuex';
 import { countObjectProperties } from '@/utils';
 
 export default {
   components: {
-    PostList
+    PostList,
+    UserProfileCard
   },
   computed: {
     ...mapGetters({
