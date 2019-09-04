f7d23aacb054baab794252f9797b8cd69cb76a82
Zetos Lima <cesar_manoel@hotmail.com>
2019-09-04 17:57:13 -0300

    post editing link

--- src/components/PostListItem.vue
+++ src/components/PostListItem.vue
@@ -11,9 +11,19 @@
     </div>
 
     <div class="post-content">
-      <div v-if="!editing">
-        <p>{{ post.text }}</p>
-      </div>
+      <template v-if="!editing">
+        <div>
+          <p>{{ post.text }}</p>
+        </div>
+        <a
+          @click.prevent="editing = true"
+          href="#"
+          style="margin-left: auto;"
+          class="link-unstyled"
+          title="Make a change"
+          ><i class="fa fa-pencil"></i
+        ></a>
+      </template>
       <div v-else>
         <PostEditor :post="post" @save="editing = false" />
       </div>
