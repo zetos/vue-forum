17c9226d2cde82472d55f266bfa4bcbbf16e77d0
Zetos Lima <cesar_manoel@hotmail.com>
2019-09-12 20:04:05 -0300

    env variables

--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import './registerServiceWorker';
-import firebaseConfig from '../firebaseConfig';
 
 // Components
 import AppDate from '@/components/AppDate';
@@ -12,9 +11,18 @@ import AppDate from '@/components/AppDate';
 Vue.config.productionTip = false;
 Vue.component('AppDate', AppDate);
 
-// console.log('fire config:', firebaseConfig);
+const fireConfig = {
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.VUE_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_FIREBASE_APP_ID
+};
+
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(fireConfig);
 
 new Vue({
   router,
