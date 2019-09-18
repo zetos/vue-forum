aeba8f7e0ec0570d1eba0cdac326e99fdbe66354
Zetos Lima <cesar_manoel@hotmail.com>
2019-09-18 00:54:06 -0300

    Fix updateUser action to use firebase.

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,4 +1,5 @@
 import firebase from 'firebase';
+import { removeEmptyShallowProps } from '@/utils';
 
 export default {
   createPost({ commit, state }, post) {
@@ -222,7 +223,27 @@ export default {
   },
 
   updateUser({ commit }, user) {
-    commit('setUser', { userId: user['.key'], user });
+    const updates = {
+      avatar: user.avatar,
+      username: user.username,
+      name: user.name,
+      bio: user.bio,
+      website: user.website,
+      email: user.email,
+      location: user.location
+    };
+
+    return new Promise(resolve => {
+      firebase
+        .database()
+        .ref('users')
+        .child(user['.key'])
+        .update(removeEmptyShallowProps(updates))
+        .then(() => {
+          commit('setUser', { userId: user['.key'], user });
+          resolve(user);
+        });
+    });
   },
 
   initAuthentication({ dispatch, commit, state }) {
